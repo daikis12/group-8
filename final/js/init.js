0a63ee431ee27d3e58663196c345163953a30b72
Angeline <angelineeow@g.ucla.edu>
2022-06-07 23:50:19 -0700

    edited card

--- final/js/init.js
+++ final/js/init.js
@@ -263,8 +263,11 @@ function focusFeature(e) {
     }
 }
 
+let count = 0
+
 function populatePanel(stateInfo){
     let data2loop = filterSurveyData(stateInfo);
+    let count = 0;
     console.log(data2loop)
     panelContent = `<div> <h3>${stateInfo}</h3><p> The experience of international students at UCLA who face career-related opportunity barriers </p </div>`
     panelContent += `<div class="pyro"><div class="before"></div><div class="after"></div></div>`
@@ -274,7 +277,14 @@ function populatePanel(stateInfo){
 
 function addtoPanel(data) {
     console.log(data)
-    panelContent += `<div class="card"> ${data["Timestamp"]} </div>`
+    count = count + 1
+    panelContent += `<div class="card">
+    <h2>Testimonial ${count}</h2>
+    <p class="left">Company name and/or job title: ${data["If applicable, what companies did/do you work under and what were/are your job titles? "]}</p>
+    <p class="left">Challenge: ${data["Have you faced any challenges finding employment/internship opportunities in the US because of your immigration status?"]}</p>
+    <p class="left">Any helpful resource: ${data["Are there any people, campus resources, or online resources that you have found helpful and/or utilized to find employment/internship opportunities in the US?"]}</p>
+    <p class="left">Advice for current international students: ${data["Do you have any advice for current international students at UCLA or is there anything else you'd like to share about your experiences? "]}</p>
+    </div>`
     document.getElementById("dietakoyaki").innerHTML = panelContent;
 }
 
